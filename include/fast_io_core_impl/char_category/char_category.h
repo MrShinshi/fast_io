67fc49dfda268a22ce7279fd2169d0827047ba0c
trcrsired <uwgghhbcad@gmail.com>
2025-09-24 05:08:33 +0800

    Fix warnings for char32_t casting

--- include/fast_io_core_impl/char_category/char_category.h
+++ include/fast_io_core_impl/char_category/char_category.h
@@ -1556,7 +1556,7 @@ namespace details
 {
 inline constexpr char32_t to_c_upper_ascii_impl(char32_t ch) noexcept
 {
-	constexpr char32_t alphanum{static_cast<char32_t>(26u)};
+	constexpr char32_t alphanum{static_cast<char32_t>(static_cast<::std::uint_least32_t>(26u))};
 	char32_t res{ch - U'a'};
 	if (res < alphanum)
 	{
@@ -1567,7 +1567,7 @@ inline constexpr char32_t to_c_upper_ascii_impl(char32_t ch) noexcept
 
 inline constexpr char32_t to_c_lower_ascii_impl(char32_t ch) noexcept
 {
-	constexpr char32_t alphanum{static_cast<char32_t>(26u)};
+	constexpr char32_t alphanum{static_cast<char32_t>(static_cast<::std::uint_least32_t>(26u))};
 	char32_t res{ch - U'A'};
 	if (res < alphanum)
 	{
@@ -1797,7 +1797,7 @@ inline constexpr bool is_html_whitespace_wide_impl(wchar_t ch) noexcept
 
 inline constexpr bool is_dos_path_invalid_character_impl(char32_t ch) noexcept
 {
-	if (ch < static_cast<char32_t>(32u))
+	if (ch < static_cast<char32_t>(static_cast<::std::uint_least32_t>(32u)))
 	{
 		return true;
 	}
@@ -1863,7 +1863,7 @@ inline constexpr bool is_c_halfwidth(char_type ch) noexcept
 	}
 	else if constexpr (!::std::same_as<char_type, char32_t> && sizeof(char_type) == sizeof(char32_t))
 	{
-		return is_c_halfwidth(static_cast<char32_t>(ch));
+		return is_c_halfwidth(static_cast<char32_t>(static_cast<::std::uint_least32_t>(ch)));
 	}
 	else if constexpr (::std::signed_integral<char_type>)
 	{
@@ -1895,7 +1895,7 @@ inline constexpr bool is_c_fullwidth(char_type ch) noexcept
 	}
 	else if constexpr (!::std::same_as<char_type, char32_t> && sizeof(char_type) == sizeof(char32_t))
 	{
-		return is_c_fullwidth(static_cast<char32_t>(ch));
+		return is_c_fullwidth(static_cast<char32_t>(static_cast<::std::uint_least32_t>(ch)));
 	}
 	else if constexpr (::std::signed_integral<char_type>)
 	{
@@ -1930,7 +1930,7 @@ inline constexpr bool is_dos_path_invalid_character(T ch) noexcept
 	}
 	else
 	{
-		return ::fast_io::char_category::details::is_dos_path_invalid_character_impl(static_cast<char32_t>(ch));
+		return ::fast_io::char_category::details::is_dos_path_invalid_character_impl(static_cast<char32_t>(static_cast<::std::uint_least32_t>(ch)));
 	}
 }
 
@@ -1943,7 +1943,7 @@ inline constexpr bool is_dos_path_invalid_prefix_character(T ch) noexcept
 	}
 	else
 	{
-		return ::fast_io::char_category::details::is_dos_path_invalid_prefix_character_impl(static_cast<char32_t>(ch));
+		return ::fast_io::char_category::details::is_dos_path_invalid_prefix_character_impl(static_cast<char32_t>(static_cast<::std::uint_least32_t>(ch)));
 	}
 }
 
