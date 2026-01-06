c9c555dbb6035835195b1c790e9ec746d5d0ce25
trcrsired <uwgghhbcad@gmail.com>
2026-01-06 20:08:51 +0800

    [skip ci] add benchmark for multi_push_back for deque by reusing vector's

---
+++ benchmark/0011.containers/deque/0002.multi_push_back/fast_io.cc
@@ -0,0 +1,8 @@
+﻿#include <fast_io_dsal/deque.h>
+namespace test
+{
+template <typename T>
+using vector = ::fast_io::deque<T>;
+}
+#define BENCH_VECTOR_COMMENT_STRING u8"fast_io::deque<T>"
+#include "main.h"
